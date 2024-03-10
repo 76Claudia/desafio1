c14b209df7919f471a26404d59ba7b63cb578091
76Claudia <claudinha.gcruz@gmail.com>
2024-03-10 00:16:25 -0300

    Seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_categoria(descricao) VALUES('Curso')
+INSERT INTO tb_categoria(descricao) VALUES('Oficina')
+
+INSERT INTO tb_atividade(nome) VALUES('Curso de HTML')
+INSERT INTO tb_atividade(descricao) VALUES('Aprenda HTML de forma pratica')
+INSERT INTO tb_atividade(preco) VALUES(80.00)
+
+INSERT INTO tb_atividade(nome) VALUES('Oficina de Github')
+INSERT INTO tb_atividade(descricao) VALUES('Controle versoes de seus projetos')
+INSERT INTO tb_atividade(preco) VALUES(50.00)
