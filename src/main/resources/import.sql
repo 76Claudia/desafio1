183da481a27223bdcb82e529c07db2437a5b6027
76Claudia <claudinha.gcruz@gmail.com>
2024-03-10 21:30:29 -0300

    Atividade Bloco Participante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,10 @@
 INSERT INTO tb_categoria(descricao) VALUES('Curso')
 INSERT INTO tb_categoria(descricao) VALUES('Oficina')
 
-INSERT INTO tb_atividade(nome) VALUES('Curso de HTML')
-INSERT INTO tb_atividade(descricao) VALUES('Aprenda HTML de forma pratica')
-INSERT INTO tb_atividade(preco) VALUES(80.00)
+INSERT INTO tb_atividade(nome, descricao, preco) VALUES('Curso de HTML', 'Aprenda HTML de forma pratica', 80.00)
+INSERT INTO tb_atividade(nome, descricao, preco) VALUES('Oficina de Github','Controle versoes de seus projetos', 50.00)
 
-INSERT INTO tb_atividade(nome) VALUES('Oficina de Github')
-INSERT INTO tb_atividade(descricao) VALUES('Controle versoes de seus projetos')
-INSERT INTO tb_atividade(preco) VALUES(50.00)
+
+INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '25-09-2017 08:00:00', '25-09-2017 11:00:00')
+INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '25-09-2017 14:00:00', '25-09-2017 18:00:00')
+INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '26-09-2017 08:00:00', '26-09-2017 11:00:00')
