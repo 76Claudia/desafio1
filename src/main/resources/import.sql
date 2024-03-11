a50763465d6328990539a0f20fcdf3b200207f6f
76Claudia <claudinha.gcruz@gmail.com>
2024-03-10 22:22:50 -0300

    Seeding

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,6 +5,11 @@ INSERT INTO tb_atividade(nome, descricao, preco) VALUES('Curso de HTML', 'Aprend
 INSERT INTO tb_atividade(nome, descricao, preco) VALUES('Oficina de Github','Controle versoes de seus projetos', 50.00)
 
 
-INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '25-09-2017 08:00:00', '25-09-2017 11:00:00')
-INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '25-09-2017 14:00:00', '25-09-2017 18:00:00')
-INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '26-09-2017 08:00:00', '26-09-2017 11:00:00')
+INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '25-09-2017T08:00:00Z', '25-09-2017T11:00:00Z')
+INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '25-09-2017T14:00:00Z', '25-09-2017T18:00:00Z')
+INSERT INTO tb_bloco (inicio, fim) VALUES (TIMESTAMP WITH TIME ZONE '26-09-2017T08:00:00Z', '26-09-2017T11:00:00Z')
+
+INSERT INTO tb_participante(nome, email) VALUES('Jose Silva', 'jose@gmail.com')
+INSERT INTO tb_participante(nome, email) VALUES('Tiago Faria', 'tiago@gmail.com')
+INSERT INTO tb_participante(nome, email) VALUES('Maria do Rosario', 'maria@gmail.com')
+INSERT INTO tb_participante(nome, email) VALUES('Teresa Silva', 'teresa@gmail.com')
